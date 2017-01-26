5c0b1c5c3f462ce423946e04f6f4ce25fb0510aa
Tyler Rocha <tyler.rocha@utexas.edu>
2017-01-26 12:51:06 +0800

    use switch in processProgramArgs

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -257,18 +257,17 @@ public class AddressBook {
      * @param args full program arguments passed to application main method
      */
     private static void processProgramArgs(String[] args) {
-        if (args.length >= 2) {
+    	switch (args.length) {
+    	case 0:
+    		setupDefaultFileForStorage();
+    		break;
+    	case 1: 
+    		setupGivenFileForStorage(args[0]);
+    		break;
+    	default:
             showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
             exitProgram();
-        }
-
-        if (args.length == 1) {
-            setupGivenFileForStorage(args[0]);
-        }
-
-        if (args.length == 0) {
-            setupDefaultFileForStorage();
-        }
+    	}
     }
 
     /**
